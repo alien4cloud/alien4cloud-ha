b8feebd5c48913889d37c94336cf0de40bcf762c
Xavier DEGENNE <xavier.degenne@fastconnect.fr>
2016-07-18 15:01:51 +0000

    [ALIEN-1638] add HaProxy

--- src/test/resources/tosca/scripts/dnsmasq/configure.sh
+++ src/test/resources/tosca/scripts/dnsmasq/configure.sh
@@ -3,4 +3,5 @@
 sudo /bin/su -c "echo 'no-resolv' > /etc/dnsmasq.conf"
 sudo /bin/su -c "echo 'no-hosts' >> /etc/dnsmasq.conf"
 sudo /bin/su -c "echo 'user=root' >> /etc/dnsmasq.conf"
+#sudo /bin/su -c "echo 'cache-size=0' >> /etc/dnsmasq.conf"
 sudo /bin/su -c "echo 'server=/consul/127.0.0.1#8600' >> /etc/dnsmasq.conf"
